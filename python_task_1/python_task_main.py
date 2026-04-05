fccc0ee5b8da5596ada3e03193a7e5a09115d3fc
Matsiuk_Dmytro_V <matsiukdmytrov@gmail.com>
2026-04-06 02:09:20 +0300

    working with folders

--- python_task_1/python_task_main.py
+++ python_task_1/python_task_main.py
@@ -1,7 +1,30 @@
 import sys
+import os
 from pathlib import Path
+import shutil
+
+def normalize(inStr):
+    return inStr
+
+def sort_file(inFileName,inFolder):
+    l_image = ['JPEG', 'PNG', 'JPG', 'SVG']
+    l_document = ['DOC', 'DOCX', 'TXT', 'PDF', 'XLSX', 'PPTX']
+    l_video = ['AVI', 'MP4', 'MOV', 'MKV']
+    l_audio = ['MP3', 'OGG', 'WAV', 'AMR']
+    l_archive = ['ZIP', 'GZ', 'TAR']
+    File_Extension_dict = {("images",l_image),("documents",l_document),("videos",l_video),("audio",l_audio),("archives",l_archive)}
+    new_Folder = ""
+    for fn,t in File_Extension_dict:
+        for tf in list(p.glob("*."+t)):
+            shutil.move(str(tf), inFolder+"\\"+fn)
+
+    for tf in list(p.glob("*.*")):
+        shutil.move(str(tf), inFolder + "\\other")
+    return 0
 
 def sort_folders(inFolder=""):
+
+
     p = Path(inFolder)
     for x in p.iterdir():
         if x.is_dir():
@@ -9,6 +32,8 @@ def sort_folders(inFolder=""):
             locFolder = inFolder+"\\"+x.name
             print(locFolder)
             sort_folders(locFolder)
+        for tf in list(p.glob("*.*")):
+
     #print([x  if x.is_dir()])
     #print(list(p.glob('*.*')))
     return 0
