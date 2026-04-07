d3048017a38e19fa4339c5e102a5ccf10b362bea
Matsiuk_Dmytro_V <matsiukdmytrov@gmail.com>
2026-04-08 01:28:42 +0300

    working

--- python_task_1/python_task_main.py
+++ python_task_1/python_task_main.py
@@ -1,40 +1,55 @@
+import string
 import sys
-import os
+#import os
 from pathlib import Path
 import shutil
 import cyrtranslit
 
-def normalize(inStr):
-    returnStr = cyrtranslit.to_latin(inStr,"ua")
-    return returnStr
+def normalize(in_str):
+    return_sr_start = cyrtranslit.to_latin(in_str,"ua")
+    return_str = ""
+    for locSymb in return_sr_start:
+        if locSymb in string.ascii_letters:
+            return_str = return_str + locSymb
+        elif locSymb in string.digits:
+            return_str = return_str + locSymb
+        else:
+            return_str = return_str + "_"
 
-def sort_file(inFileName,inFolder):
+    return return_str
+
+def sort_files(in_folder):
     l_image = ['JPEG', 'PNG', 'JPG', 'SVG']
     l_document = ['DOC', 'DOCX', 'TXT', 'PDF', 'XLSX', 'PPTX']
     l_video = ['AVI', 'MP4', 'MOV', 'MKV']
     l_audio = ['MP3', 'OGG', 'WAV', 'AMR']
     l_archive = ['ZIP', 'GZ', 'TAR']
-    File_Extension_dict = {("images",l_image),("documents",l_document),("videos",l_video),("audio",l_audio),("archives",l_archive)}
-    new_Folder = ""
-    for fn,t in File_Extension_dict:
-        for tf in list(p.glob("*."+t)):
-            shutil.move(str(tf), inFolder+"\\"+fn)
-
+    file_extension_dict = {("images",l_image),("documents",l_document),("videos",l_video),("audio",l_audio),("archives",l_archive)}
+    #new_Folder = ""
+    p = Path(in_folder)
+    for fn,t in file_extension_dict:
+        for locExt in t:
+            for tf in list(p.glob("*."+locExt)):
+                if fn=="archives":
+                    shutil.unpack_archive(str(tf),in_folder+"\\"+fn)
+                else:
+                    shutil.move(str(tf), in_folder+"\\"+fn)
     for tf in list(p.glob("*.*")):
-        shutil.move(str(tf), inFolder + "\\other")
+        shutil.move(str(tf), in_folder + "\\other")
     return 0
 
-def sort_folders(inFolder=""):
-
-
-    p = Path(inFolder)
+def sort_folders(in_folder=""):
+    p = Path(in_folder)
     for x in p.iterdir():
         if x.is_dir():
             #print(x.name)
-            locFolder = inFolder+"\\"+x.name
-            print(locFolder)
-            sort_folders(locFolder)
-        for tf in list(p.glob("*.*")):
+            loc_folder = in_folder+"\\"+x.name
+            print(loc_folder)
+            sort_folders(loc_folder)
+        sort_files(in_folder)
+        #for tf in list(p.glob("*.*")):
+        #    sort_file(tf,in_folder)
+
 
     #print([x  if x.is_dir()])
     #print(list(p.glob('*.*')))
