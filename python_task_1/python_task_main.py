dd21f49f9b4bee42dab1e0168889f8ef95786050
Matsiuk_Dmytro_V <matsiukdmytrov@gmail.com>
2026-04-13 10:44:47 +0300

    working

--- python_task_1/python_task_main.py
+++ python_task_1/python_task_main.py
@@ -7,11 +7,12 @@ import shutil
 #import cyrtranslit
 
 other_folder = Path()
+trans_table = list()
+file_extension_dict = dict(Path:list)
 
-def normalize(in_str="",in_trans_table=[]):
-    #return_sr_start = cyrtranslit.to_latin(in_str,"ua")
-
-    return_sr_start = in_str.translate(in_trans_table)
+def normalize(in_str:str):
+    global trans_table
+    return_sr_start = in_str.translate(trans_table)
     return_str = ""
     for locSymb in return_sr_start:
         if locSymb in string.ascii_letters:
@@ -23,7 +24,8 @@ def normalize(in_str="",in_trans_table=[]):
 
     return return_str
 
-def normalize_names_in_folders(in_folder,trans_table):
+def normalize_names_in_folders(in_folder:Path):
+    global trans_table
     for internal_object in in_folder.iterdir():
         if internal_object.is_dir():
             normalize_names_in_folders(internal_object,trans_table)
@@ -38,7 +40,8 @@ def normalize_names_in_folders(in_folder,trans_table):
             internal_object.rename(new_file)
     return 0
 
-def sort_files(in_folder,file_extension_dict={}):
+def sort_files(in_folder:Path):
+    global file_extension_dict
     for file_extension_folder in file_extension_dict:
         file_extension_list = file_extension_dict[file_extension_folder]
         for loc_ext in file_extension_list:
@@ -58,7 +61,8 @@ def sort_files(in_folder,file_extension_dict={}):
             tf.move_into(other_folder)
     return 0
 
-def sort_folders(in_folder,file_extension_dict={}):
+def sort_folders(in_folder:Path):
+    global file_extension_dict
     for internal_object in in_folder.iterdir():
         if internal_object.absolute() in file_extension_dict or internal_object.absolute() == other_folder:
             continue
@@ -66,6 +70,43 @@ def sort_folders(in_folder,file_extension_dict={}):
             sort_folders(internal_object,file_extension_dict)
             internal_object.rmdir()
     sort_files(in_folder, file_extension_dict)
+    return 0
+
+def init_global_variables(in_litter_folder:str):
+    global other_folder
+    global trans_table
+    global file_extension_dict
+
+    l_image = ['JPEG', 'PNG', 'JPG', 'SVG']
+    l_document = ['DOC', 'DOCX', 'TXT', 'PDF', 'XLSX', 'PPTX']
+    l_video = ['AVI', 'MP4', 'MOV', 'MKV']
+    l_audio = ['MP3', 'OGG', 'WAV', 'AMR']
+    l_archive = ['ZIP', 'GZ', 'TAR']
+
+    images_folder = Path(in_litter_folder + "\\images")
+    documents_folder = Path(in_litter_folder + "\\documents")
+    videos_folder = Path(in_litter_folder + "\\videos")
+    audio_folder = Path(in_litter_folder + "\\audio")
+    archives_folder = Path(in_litter_folder + "\\archives")
+    other_folder = Path(in_litter_folder + "\\other")
+
+    file_extension_dict = {images_folder: l_image, documents_folder: l_document, videos_folder: l_video,
+                           audio_folder: l_audio, archives_folder: l_archive}
+
+    trans_dict = {
+        'а': 'a', 'б': 'b', 'в': 'v', 'г': 'h', 'ґ': 'g', 'д': 'd', 'е': 'e', 'є': 'ye',
+        'ж': 'zh', 'з': 'z', 'и': 'y', 'і': 'i', 'ї': 'yi', 'й': 'y', 'к': 'k', 'л': 'l',
+        'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u',
+        'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch', 'ь': '',
+        'ю': 'yu', 'я': 'ya',
+
+        'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'H', 'Ґ': 'G', 'Д': 'D', 'Е': 'E', 'Є': 'Ye',
+        'Ж': 'Zh', 'З': 'Z', 'И': 'Y', 'І': 'I', 'Ї': 'Yi', 'Й': 'Y', 'К': 'K', 'Л': 'L',
+        'М': 'M', 'Н': 'N', 'О': 'O', 'П': 'P', 'Р': 'R', 'С': 'S', 'Т': 'T', 'У': 'U',
+        'Ф': 'F', 'Х': 'Kh', 'Ц': 'Ts', 'Ч': 'Ch', 'Ш': 'Sh', 'Щ': 'Shch', 'Ь': '',
+        'Ю': 'Yu', 'Я': 'Ya'
+    }
+    trans_table = str.maketrans(trans_dict)
     return 0
 
 def main():
@@ -87,42 +128,13 @@ def main():
 
     #print(f"Starting to sort: {folder_path.absolute()}")
 
-    global other_folder
     litter_folder = input("Input litter folder:")
+    init_global_variables(litter_folder)
 
-    l_image = ['JPEG', 'PNG', 'JPG', 'SVG']
-    l_document = ['DOC', 'DOCX', 'TXT', 'PDF', 'XLSX', 'PPTX']
-    l_video = ['AVI', 'MP4', 'MOV', 'MKV']
-    l_audio = ['MP3', 'OGG', 'WAV', 'AMR']
-    l_archive = ['ZIP', 'GZ', 'TAR']
-
-    images_folder = Path(litter_folder + "\\images")
-    documents_folder = Path(litter_folder + "\\documents")
-    videos_folder = Path(litter_folder + "\\videos")
-    audio_folder = Path(litter_folder + "\\audio")
-    archives_folder = Path(litter_folder + "\\archives")
-
-    file_extension_dict = {images_folder:l_image,documents_folder:l_document,videos_folder:l_video,audio_folder:l_audio,archives_folder:l_archive}
-
-    other_folder  = Path(litter_folder + "\\other")
     litter_folder_path = Path(litter_folder)
-    sort_folders(litter_folder_path,file_extension_dict)
 
-    trans_dict = {
-        'а': 'a', 'б': 'b', 'в': 'v', 'г': 'h', 'ґ': 'g', 'д': 'd', 'е': 'e', 'є': 'ye',
-        'ж': 'zh', 'з': 'z', 'и': 'y', 'і': 'i', 'ї': 'yi', 'й': 'y', 'к': 'k', 'л': 'l',
-        'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u',
-        'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch', 'ь': '',
-        'ю': 'yu', 'я': 'ya',
-
-        'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'H', 'Ґ': 'G', 'Д': 'D', 'Е': 'E', 'Є': 'Ye',
-        'Ж': 'Zh', 'З': 'Z', 'И': 'Y', 'І': 'I', 'Ї': 'Yi', 'Й': 'Y', 'К': 'K', 'Л': 'L',
-        'М': 'M', 'Н': 'N', 'О': 'O', 'П': 'P', 'Р': 'R', 'С': 'S', 'Т': 'T', 'У': 'U',
-        'Ф': 'F', 'Х': 'Kh', 'Ц': 'Ts', 'Ч': 'Ch', 'Ш': 'Sh', 'Щ': 'Shch', 'Ь': '',
-        'Ю': 'Yu', 'Я': 'Ya'
-    }
-    trans_table = str.maketrans(trans_dict)
-    normalize_names_in_folders(litter_folder_path, trans_table)
+    sort_folders(litter_folder_path)
+    normalize_names_in_folders(litter_folder_path)
     return 0
 
 if __name__ == "__main__":
